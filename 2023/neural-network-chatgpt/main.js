29933998cb1399c9d7305b05ee6e43b909923506
hoang.tran12 <hoang.tran12@mservice.com.vn>
2023-03-06 18:07:43 +0700

    .

--- 2023/neural-network-chatgpt/main.js
+++ 2023/neural-network-chatgpt/main.js
@@ -17,18 +17,28 @@ window.setup = () => {
     inputs: data.map((d) => d.inputs),
     targets: data.map((d) => (d.label == 1 ? [0, 1] : [1, 0])),
   };
+
+  // train the neural network
+  neuralNetwork.train(trainingData.inputs, trainingData.targets, {
+    epochs: 1,
+    learningRate: 0.1,
+  });
 };
+
+function classifyOutput(output) {
+  return output.indexOf(Math.max(...output));
+}
 
 window.draw = () => {
   background(30);
 
   // draw training data
-  stroke(255);
-  strokeWeight(1);
+  noStroke();
   for (let i = 0; i < trainingData.inputs.length; i++) {
     let x = map(trainingData.inputs[i][0], -graphSize, graphSize, 0, width);
     let y = map(trainingData.inputs[i][1], -graphSize, graphSize, height, 0);
-    if (trainingData.targets[i] === 0) {
+    let classification = classifyOutput(trainingData.targets[i]);
+    if (classification === 0) {
       fill(255, 0, 0);
     } else {
       fill(0, 0, 255);
@@ -45,8 +55,9 @@ window.draw = () => {
       let graphY = map(y, 0, height, graphSize, -graphSize);
 
       // draw the graph
-      let classification = neuralNetwork.classify([graphX, graphY]);
-      if (classification === 0) {
+      let output = neuralNetwork.feedForward([graphX, graphY]);
+      let predicted = classifyOutput(output);
+      if (predicted === 0) {
         pixels[index + 0] = 255;
         pixels[index + 1] = 0;
         pixels[index + 2] = 0;
@@ -72,15 +83,27 @@ window.draw = () => {
   fill(255);
   noStroke();
   textSize(16);
-  text(`Loss: ${loss}`, 10, 20);
+  text(`Loss: ${loss.toFixed(5)}`, 10, 20);
 
   // // draw correct count
   let correctCount = 0;
   for (let i = 0; i < trainingData.inputs.length; i++) {
-    let predicted = neuralNetwork.classify(trainingData.inputs[i]);
-    let expected = trainingData.targets[i];
-    if (predicted == expected) {
+    let input = trainingData.inputs[i];
+    let output = neuralNetwork.feedForward(input);
+
+    let predicted = classifyOutput(output);
+    let expected = classifyOutput(trainingData.targets[i]);
+
+    if (predicted === expected) {
       correctCount++;
+
+      // highlight the correct ones
+      let x = map(input[0], -graphSize, graphSize, 0, width);
+      let y = map(input[1], -graphSize, graphSize, height, 0);
+      stroke(0, 255, 0);
+      strokeWeight(2);
+      noFill();
+      ellipse(x, y, 8);
     }
   }
   fill(255);
@@ -100,8 +123,6 @@ class NeuralNetwork {
         new Layer(layerSizes[i], layerSizes[i + 1], activationFunction)
       );
     }
-
-    this.outputsCache = null;
   }
 
   feedForward(inputs) {
@@ -112,18 +133,69 @@ class NeuralNetwork {
     return outputs;
   }
 
-  classify(inputs) {
-    let outputs = this.feedForward(inputs);
-    return outputs.indexOf(Math.max(...outputs));
-  }
-
   loss(inputs, targets) {
     let loss = 0;
     for (let i = 0; i < inputs.length; i++) {
       let outputs = this.feedForward(inputs[i]);
-      loss += this.lossFunction.activate(outputs, targets[i]);
+      loss += this.lossFunction.calculate(outputs, targets[i]);
     }
     return loss / inputs.length;
+  }
+
+  static splitTrainTest(inputs, targets, trainPercentage) {
+    let trainingData = {
+      inputs: [],
+      targets: [],
+    };
+    let testingData = {
+      inputs: [],
+      targets: [],
+    };
+
+    for (let i = 0; i < inputs.length; i++) {
+      if (random() < trainPercentage) {
+        trainingData.inputs.push(inputs[i]);
+        trainingData.targets.push(targets[i]);
+      } else {
+        testingData.inputs.push(inputs[i]);
+        testingData.targets.push(targets[i]);
+      }
+    }
+
+    return [trainingData, testingData];
+  }
+
+  trainBatch(inputs, targets, learningRate) {
+    for (let layer of this.layers) {
+    }
+  }
+
+  train(inputs, targets, options) {
+    options = {
+      learningRate: 0.1,
+      epochs: 1,
+      batchSize: 1,
+      trainPercentage: 0.8,
+      ...options,
+    };
+
+    let [trainingData, testingData] = NeuralNetwork.splitTrainTest(
+      inputs,
+      targets,
+      options.trainPercentage
+    );
+
+    for (let epoch = 0; epoch < options.epochs; epoch++) {
+      for (let i = 0; i < trainingData.inputs.length; i += options.batchSize) {
+        let inputsBatch = trainingData.inputs.slice(i, i + options.batchSize);
+        let targetsBatch = trainingData.targets.slice(i, i + options.batchSize);
+
+        this.trainBatch(inputsBatch, targetsBatch, options.learningRate);
+      }
+
+      let loss = this.loss(testingData.inputs, testingData.targets);
+      console.log(`Epoch ${epoch}: loss = ${loss}`);
+    }
   }
 }
 
@@ -136,19 +208,21 @@ class Layer {
     this.weights = [];
     this.biases = [];
 
-    // initialize the weights and biases
-    for (let i = 0; i < numOutputs; i++) {
-      this.weights.push([]);
-      for (let j = 0; j < numInputs; j++) {
-        this.weights[i].push(random(-1, 1));
+    this.initializeWeightsAndBiases();
+  }
+
+  initializeWeightsAndBiases() {
+    for (let i = 0; i < this.numOutputs; i++) {
+      this.biases[i] = 0;
+      this.weights[i] = [];
+      for (let j = 0; j < this.numInputs; j++) {
+        this.weights[i][j] = Math.random() * 2 - 1;
       }
-      this.biases.push(random(-1, 1));
     }
   }
 
   feedForward(inputs) {
-    this.inputs = inputs;
-    this.outputs = [];
+    let outputs = [];
 
     for (let i = 0; i < this.numOutputs; i++) {
       let sum = 0;
@@ -156,10 +230,35 @@ class Layer {
         sum += this.weights[i][j] * inputs[j];
       }
       sum += this.biases[i];
-      this.outputs.push(this.activationFunction.activate(sum));
+      outputs.push(this.activationFunction.activate(sum));
     }
 
-    return this.outputs;
+    return outputs;
+  }
+
+  backpropagate(errors, learningRate, inputs) {
+    // calculate the new errors
+    let newErrors = [];
+    for (let i = 0; i < this.numInputs; i++) {
+      let sum = 0;
+      for (let j = 0; j < this.numOutputs; j++) {
+        sum += this.weights[j][i] * errors[j];
+      }
+      newErrors[i] = sum;
+    }
+
+    // update the weights and biases
+    for (let i = 0; i < this.numOutputs; i++) {
+      for (let j = 0; j < this.numInputs; j++) {
+        this.weights[i][j] -=
+          learningRate *
+          errors[i] *
+          this.activationFunction.derivative(inputs[j]);
+      }
+      this.biases[i] -= learningRate * errors[i];
+    }
+
+    return newErrors;
   }
 }
 
@@ -172,7 +271,7 @@ class ActivationFunction {
       return 1 / (1 + Math.exp(-x));
     },
     derivative: (x) => {
-      const sigmoidX = ActivationFunction.sigmoid.activate(x);
+      const sigmoidX = ActivationFunction.Sigmoid.activate(x);
       return sigmoidX * (1 - sigmoidX);
     },
   };
@@ -185,7 +284,7 @@ class ActivationFunction {
       return (Math.exp(x) - Math.exp(-x)) / (Math.exp(x) + Math.exp(-x));
     },
     derivative: (x) => {
-      const tanhX = ActivationFunction.tanh.activate(x);
+      const tanhX = ActivationFunction.Tanh.activate(x);
       return 1 - tanhX * tanhX;
     },
   };
@@ -224,7 +323,7 @@ class ActivationFunction {
       return exps.map((val) => val / sumExps);
     },
     derivative: (x) => {
-      const softmaxX = ActivationFunction.softmax.activate(x);
+      const softmaxX = ActivationFunction.Softmax.activate(x);
       return softmaxX.map((val) => val * (1 - val));
     },
   };
@@ -235,7 +334,7 @@ class LossFunction {
   // where the output is a continuous value. The MSE computes the average of
   // the squared differences between the predicted output and the actual output.
   static MeanSquaredError = {
-    activate: (predicted, actual) => {
+    calculate: (predicted, actual) => {
       let sum = 0;
       for (let i = 0; i < predicted.length; i++) {
         sum += Math.pow(predicted[i] - actual[i], 2);
@@ -256,7 +355,7 @@ class LossFunction {
   // The cross-entropy loss measures the difference between the predicted
   // probability distribution and the actual distribution.
   static CrossEntropyLoss = {
-    activate: (predicted, actual) => {
+    calculate: (predicted, actual) => {
       let loss = 0;
       for (let i = 0; i < predicted.length; i++) {
         loss += actual[i] * Math.log(predicted[i]);
@@ -275,7 +374,7 @@ class LossFunction {
   // Binary Cross-Entropy Loss: This is a special case of the cross-entropy loss
   // used for binary classification problems, where the output is either 0 or 1.
   static BinaryCrossEntropyLoss = {
-    activate: (predicted, actual) => {
+    calculate: (predicted, actual) => {
       return (
         -actual * Math.log(predicted) - (1 - actual) * Math.log(1 - predicted)
       );
@@ -288,7 +387,7 @@ class LossFunction {
   // Categorical Cross-Entropy Loss: This is a variant of the cross-entropy loss
   // used for multiclass classification problems, where the output is a probability distribution over several classes.
   static CategoricalCrossEntropyLoss = {
-    activate: (predicted, actual) => {
+    calculate: (predicted, actual) => {
       let loss = 0;
       for (let i = 0; i < predicted.length; i++) {
         loss += actual[i] * Math.log(predicted[i]);
